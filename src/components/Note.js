ed5d5aae5d9755ab2c3461b80214b1b2aea0958a
Mbaye Ndiaye <mbayefebruary3@yahoo.com>
2021-03-14 13:19:00 -0700

    third commit

--- src/components/Note.js
+++ src/components/Note.js
@@ -9,7 +9,7 @@ const Note = () => {
 
 
             <p>Quick note: the service options only work for sender country (United States) and receiver country (Senegal).</p>
-            <p>The service options will enable you to check for estimates for the following: moneyGram, westerUnion, and Ria.</p>
+            <p>The service options will enable you to check for estimates for the following services: moneyGram, westerUnion, and Ria.</p>
             <br />
         </div>
     )
